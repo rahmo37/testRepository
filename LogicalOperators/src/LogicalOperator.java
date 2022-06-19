4fb244ebf6a23b6dd6039c00cce5630f05955a15
Obaedur Rahman <arnobahemd12345@gmail.com>
2022-06-19 16:16:40 -0400

    change

--- LogicalOperators/src/LogicalOperator.java
+++ LogicalOperators/src/LogicalOperator.java
@@ -47,7 +47,7 @@ public class LogicalOperator {
 				System.out.println("Who is you favorite Bangladeshi neta??");
 				response = sc.next();
 			}
-			System.out.println("Bye now!!!");
+			System.out.println("Bye Bye now!!!");
 
 		}
 
