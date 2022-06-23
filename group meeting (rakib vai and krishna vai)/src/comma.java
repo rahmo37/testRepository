a4aec297b3b84822674aeca7ad370aa0436ef292
Obaedur Rahman <arnobahemd12345@gmail.com>
2022-06-22 21:48:13 -0400

    comma problem

--- group meeting (rakib vai and krishna vai)/src/comma.java
+++ group meeting (rakib vai and krishna vai)/src/comma.java
@@ -4,22 +4,19 @@ public class comma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] s = {"a","b","c","d"};
+		String[] s = { "a", "b", "c", "d" };
 		ArrayList<Character> comma = new ArrayList<>();
-		
-		for(int a = 0; a < s.length; a++) {
+
+		for (int a = 0; a < s.length; a++) {
 			comma.add(',');
 		}
-		
-		comma.set(s.length-1, '.');
-		
-		for(int i = 0; i < s.length; i ++) {
-			System.out.print(s[i]);
-			for(int j = 0; j<1;j++) {
-				System.out.print(comma.get(i));
-			}
-		}
 
+		comma.set(s.length - 1, '.');
+
+		for (int i = 0; i < s.length; i++) {
+			System.out.print(s[i]);
+			System.out.print(comma.get(i));
+		}
 	}
 
 }
